5f3d9a2d1f4636163174248dd98664335abd0891
Alexey Smirnov <132341096+nitro807@users.noreply.github.com>
2025-07-23 11:32:22 +0300

    Refine name filter

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from app.zabbix_api import get_hosts_by_group, get_hosts_by_groups, get_all_groups, zabbix_login
+from app.zabbix_api import (
+    get_hosts_by_group,
+    get_hosts_by_groups,
+    get_all_groups,
+    zabbix_login,
+)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     try:
         auth = zabbix_login()
-        hosts = get_hosts_by_groups(["gr3","Xfit"], auth)
+        hosts = get_hosts_by_groups(["gr3", "Xfit"], auth, name_filter="gr3")
     except Exception as e:
         print(f"[MAIN] Ошибка в index: {e}")
         return templates.TemplateResponse(
@@ -24,6 +30,7 @@ async def index(request: Request):
         "channel_status.html",
         {"request": request, "hosts": hosts}
     )
+
 
 @app.get("/groups", response_class=HTMLResponse)
 async def groups(request: Request):
